678c144fab2599f0ef256921a0ad153394f5d0f6
Dng16 <dongyang0016@gmail.com>
2023-05-17 17:53:41 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelector('.menu-links');
+
+menu.addEventListener('click',() => {
+  console.log(menu
+    );
+    menuLinks.classList.toggle('show') ;
+})
